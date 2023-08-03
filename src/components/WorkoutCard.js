0eca97e3a6d255f8b7456d49577d616ef7d8a327
Moringa School <chepfaith18@gmail.com>
2023-08-03 17:18:20 +0300

    imported necessary components

---
+++ src/components/WorkoutCard.js
@@ -0,0 +1,10 @@
+// Import React library and useState hook for managing state
+import React, { useState } from "react";
+import { Card, Stack } from "@mui/material";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import ShareBtn from "./ShareBtn";
+import LearnMore from "./LearnMore";
