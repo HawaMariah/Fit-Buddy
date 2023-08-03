eb13e84f86f5a0a0c8ce1d5dfbcd52791594aba9
Moringa School <chepfaith18@gmail.com>
2023-08-03 16:52:00 +0300

    funtion to handle opening and closing menu

--- src/components/ShareBtn.js
+++ src/components/ShareBtn.js
@@ -14,3 +14,20 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import RedditIcon from '@mui/icons-material/Reddit';
 import LinkIcon from '@mui/icons-material/Link';
+
+// Define the functional component named DropdownShareButton
+export default function DropdownShareButton() {
+    // Declare state variables for managing the menu's anchor element and its open state
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const openBtn = Boolean(anchorEl);
+  
+    // Function to handle opening the menu
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    // Function to handle closing the menu
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+  
