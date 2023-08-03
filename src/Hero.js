1ebbaf720956c554540211a4a8b975368030ba35
Moringa School <chepfaith18@gmail.com>
2023-08-03 16:34:44 +0300

    Stating variables to manage user inputs and data

--- src/Hero.js
+++ src/Hero.js
@@ -1,6 +1,18 @@
-// Import necessary hooks and components from React and Material-UI
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 import { Box, Button, Container, Paper, Stack } from "@mui/material";
 import WorkoutCard from "./components/WorkoutCard";
+
+// Define the functional component named Hero
+function Hero() {
+    // State variables to manage user inputs and data
+    const [bodyPartDD, setBodyPartDD] = useState(new Set());
+    const [muscleTypeDD, setMuscleTypeDD] = useState(new Set());
+    const [bodyPart, setBodyPart] = useState('');
+    const [muscleType, setMuscleType] = useState('');
+    const [workOuts, setWorkOuts] = useState([]);
+    const [workOutCard, setWorkOutCard] = useState([]);
+    const [equipmentDD, setEquipmentDD] = useState(new Set());
+    const [equipment, setEquipment] = useState('');
+  
