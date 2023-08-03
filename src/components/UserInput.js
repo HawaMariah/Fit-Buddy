4061799ffb395778392e8b59c6a840be43da994a
Moringa School <chepfaith18@gmail.com>
2023-08-03 17:13:03 +0300

    imported components

---
+++ src/components/UserInput.js
@@ -0,0 +1,7 @@
+// Import React library and Material-UI components
+import React from "react";
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
