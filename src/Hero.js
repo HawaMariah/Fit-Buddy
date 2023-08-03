2431b287fd2aa3b683874604566fc475da73734b
Moringa School <chepfaith18@gmail.com>
2023-08-03 16:33:46 +0300

    Import necessary hooks and components from React and Material-UI

---
+++ src/Hero.js
@@ -0,0 +1,6 @@
+// Import necessary hooks and components from React and Material-UI
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import { Box, Button, Container, Paper, Stack } from "@mui/material";
+import WorkoutCard from "./components/WorkoutCard";
