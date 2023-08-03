37845da8b14e67726835ce6e954231145e020a13
Moringa School <chepfaith18@gmail.com>
2023-08-03 16:44:27 +0300

    importing necessary components

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+// Import the React library to use JSX syntax and create React components
+import React from 'react';
+
+// Import the custom CSS styles for the Header component
+import './Header.css';
