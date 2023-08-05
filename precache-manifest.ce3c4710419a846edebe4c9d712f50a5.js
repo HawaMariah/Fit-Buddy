af01260f57693d6ae231eb3e5d768cc1842ed9c5
Hawa <hawamaria33@gmail.com>
2023-08-05 13:23:07 +0300

    Updates

---
+++ precache-manifest.ce3c4710419a846edebe4c9d712f50a5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1613bb4959288855c5e",
+    "url": "/Fit-Buddy/static/css/main.c0054f87.chunk.css"
+  },
+  {
+    "revision": "e1613bb4959288855c5e",
+    "url": "/Fit-Buddy/static/js/main.80b0291d.chunk.js"
+  },
+  {
+    "revision": "d95e7737f7a9b4b30ab9",
+    "url": "/Fit-Buddy/static/js/runtime~main.5ad5c129.js"
+  },
+  {
+    "revision": "7d48542e157f3989ce9c",
+    "url": "/Fit-Buddy/static/css/2.57d4d99a.chunk.css"
+  },
+  {
+    "revision": "7d48542e157f3989ce9c",
+    "url": "/Fit-Buddy/static/js/2.3f4e7b38.chunk.js"
+  },
+  {
+    "revision": "005c9aa92b564b73b7582cc4f1fa49cb",
+    "url": "/Fit-Buddy/static/media/fa-brands-400.005c9aa9.woff2"
+  },
+  {
+    "revision": "bb8cd014d7a55672934233c354e1c4a3",
+    "url": "/Fit-Buddy/static/media/fa-brands-400.bb8cd014.ttf"
+  },
+  {
+    "revision": "747442fa76f1d9a31f9a54a2e8a4b448",
+    "url": "/Fit-Buddy/static/media/fa-regular-400.747442fa.woff2"
+  },
+  {
+    "revision": "0248ab19e74fc3731de14d647db4687a",
+    "url": "/Fit-Buddy/static/media/fa-solid-900.0248ab19.ttf"
+  },
+  {
+    "revision": "675809e48e35c47d51c7d6fcc687ee28",
+    "url": "/Fit-Buddy/static/media/fa-regular-400.675809e4.ttf"
+  },
+  {
+    "revision": "f20773a753bf1fbe9ccec04f90a470ea",
+    "url": "/Fit-Buddy/static/media/fa-v4compatibility.f20773a7.woff2"
+  },
+  {
+    "revision": "d5e647388e2415268b700d3df2e30a0d",
+    "url": "/Fit-Buddy/static/media/fa-solid-900.d5e64738.woff2"
+  },
+  {
+    "revision": "58f1253e5639d508a6fe310c97327f82",
+    "url": "/Fit-Buddy/static/media/fa-v4compatibility.58f1253e.ttf"
+  },
+  {
+    "revision": "2b5a4a859e94f541be26b231bf27ad1d",
+    "url": "/Fit-Buddy/static/media/home.2b5a4a85.jpg"
+  },
+  {
+    "revision": "ea3ef1c38eef69f7dae4376d5625e218",
+    "url": "/Fit-Buddy/index.html"
+  }
+];
