949c3eb0b2f704fe3de95abcd75aa0bc4970f06c
Hawa <hawamaria33@gmail.com>
2023-08-05 15:22:11 +0300

    new updates

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from 'react-router-dom';
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+     <HashRouter>
+    <App />
+  </HashRouter>
+  </React.StrictMode>
 );
