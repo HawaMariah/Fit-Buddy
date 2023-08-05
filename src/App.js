11c2429456eb5e7f1eda496e2894f32dcfc28768
Hawa <hawamaria33@gmail.com>
2023-08-05 14:46:18 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
